caab018ffd825d1110f19ceaf0935debb81acd29
dnetadmin <admin@decenternet.com>
2024-04-17 00:03:54 +0000

    Removed the Vercel deploy button.

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,5 +1,5 @@
 // ProtectedPage.tsx
-//import DeployButton from "@/components/DeployButton";
+
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
@@ -20,22 +20,6 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      
-      <div className="w-full">
-       
-        
-        {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          
-          {/* <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
-            <AuthButton />
-            {/* Replace the button with the ClientCheckoutButton component */}
-            {/* <ClientCheckoutButton />
-          </div> */} */
-        
-        {/* </nav> */} */
-      
-      </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
