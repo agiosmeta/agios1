cb8a109727727a517c4fe448c767290fbeba205b
dnetadmin <admin@decenternet.com>
2024-04-17 00:18:40 +0000

    UI - page.tsx

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,5 +1,5 @@
 // ProtectedPage.tsx
-
+import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
@@ -20,28 +20,42 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="w-full">
+        <div className="py-6 font-bold bg-purple-950 text-center">
+          User Logged In View
+        </div>
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+            <DeployButton />
+            <AuthButton />
+            {/* Replace the button with the ClientCheckoutButton component */}
+            <ClientCheckoutButton />
+          </div>
+        </nav>
+      </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
+          <h2 className="font-bold text-4xl mb-4">Next</h2>
           <FetchDataSteps />
         </main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          © 2024{" "}
           <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="https://agios1-7ukrdvmxv-agios.vercel.app/"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
             >
-              Supabase
+              AGIOS
             </a>
           </p>
         </footer>
       </div>
     );
-  }  
+  }
+  
